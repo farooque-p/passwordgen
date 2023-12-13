c2fcde84897df72a709d53820ee4a39d3995ad42
Farooque Parvez <farooqueparvez@gmail.com>
2023-12-13 23:40:54 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,153 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useEffect, useState } from "react";
+
+import Slider from "@react-native-community/slider";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Card, Checkbox } from "react-native-paper";
+
+import * as Clipboard from "expo-clipboard";
 
 export default function App() {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(8);
+  const [useNumbers, setUseNumbers] = useState(false);
+  const [useSpecialCharacters, setUseSpecialCharacters] = useState(false);
+  const [copiedToClipboard, setCopyToClipboard] = useState(false);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (useNumbers) {
+      str += "0123456789";
+    }
+    if (useSpecialCharacters) {
+      str += "!@#$%^&*-_+=[]{}~`";
+    }
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, useNumbers, useSpecialCharacters, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, useNumbers, useSpecialCharacters, passwordGenerator]);
+
+  const copyPasswordToClipboard = async () => {
+    await Clipboard.setStringAsync(password);
+    setCopyToClipboard(true);
+  };
+
+  setTimeout(() => {
+    setCopyToClipboard(false);
+  }, 5000);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Text
+        style={{
+          fontSize: 24,
+          color: "white",
+          fontWeight: "600",
+          marginBottom: 30,
+        }}
+      >
+        Password Generator
+      </Text>
+      <Card>
+        <Card.Content>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 20,
+            }}
+          >
+            <TextInput
+              editable={false}
+              style={{
+                width: 250,
+                backgroundColor: "white",
+                padding: 10,
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 0,
+                borderBottomLeftRadius: 8,
+                borderBottomRightRadius: 0,
+              }}
+              value={password}
+              placeholder="Password"
+            />
+            <TouchableOpacity
+              onPress={copyPasswordToClipboard}
+              style={{
+                backgroundColor: "blue",
+                paddingHorizontal: 20,
+                paddingVertical: 13,
+                borderTopLeftRadius: 0,
+                borderTopRightRadius: 8,
+                borderBottomLeftRadius: 0,
+                borderBottomRightRadius: 8,
+              }}
+            >
+              {copiedToClipboard ? (
+                <Text style={{ color: "white", fontSize: 16 }}>Copied</Text>
+              ) : (
+                <Text style={{ color: "white", fontSize: 16 }}>Copy</Text>
+              )}
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Slider
+              style={{ width: 200, height: 40 }}
+              minimumValue={8}
+              maximumValue={24}
+              step={1}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+              onValueChange={(value) => {
+                setLength(value);
+              }}
+            />
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>
+              Length: {length}
+            </Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox
+              status={useNumbers ? "checked" : "unchecked"}
+              onPress={() => {
+                setUseNumbers((prev) => !prev);
+              }}
+            />
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>Use Numbers</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox
+              status={useSpecialCharacters ? "checked" : "unchecked"}
+              onPress={() => {
+                setUseSpecialCharacters((prev) => !prev);
+              }}
+            />
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>
+              Use Special Characters
+            </Text>
+          </View>
+        </Card.Content>
+      </Card>
     </View>
   );
 }
@@ -13,8 +155,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Password Generator App
+
+An easy-to-use random password generator. If checked, can include numbers and special characters. Generates password between 8 to 24 characters. Generated password can be copied to clipboard.
+
+
+
+
+
+
+## Screenshots
+
+![App Screenshot](https://res.cloudinary.com/duivztddr/image/upload/v1702490881/pwgen_vhlyst.png)
+
+
+
+## Features
+
+- Generates random password
+- If checked, uses numbers and special characters
+- Generated password can be copied to clipboard
+
+
+## Authors
+
+- https://github.com/farooque-p
+
+
